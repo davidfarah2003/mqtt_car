a787b11a1486eb36138efddbf2272220cf225774
davidf <david.farah@epfl.ch>
2023-02-28 22:35:38 +0100

    something

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,49 @@
 <template>
-  {{dataObject}}
+  <section>
+    <div class="form-box">
+
+      <div class="form-value">
+        <form action="">
+          <h2>Login</h2>
+          <div class="inputbox">
+            <ion-icon name="mail-outline"></ion-icon>
+            <input type="email" required>
+            <label for="">Email</label>
+          </div>
+          <div class="inputbox">
+            <ion-icon name="lock-closed-outline"></ion-icon>
+            <input type="password" required>
+            <label for="">Password</label>
+          </div>
+          <div class="forget">
+            <label for=""><input type="checkbox">Remember Me  <a href="#">Forget Password</a></label>
+
+          </div>
+          <button>Log in</button>
+          <div class="register">
+            <p>Don't have a account <a href="#">Register</a></p>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script>
+<script lang="ts">
 
 import {inject, ref} from "vue";
 import mqttUtils from "@/composables/mqttUtils.mjs";
+import {Joystick} from "vue-joystick-component";
 
 export default {
   name: "Home",
-  components: {},
+  components: {Joystick},
+
   setup(){
     let dataObject = ref({
-      test: {
-        test2 : 1
-      }
-    }
-    )
+      current_speed : 0,
+      current_rotation : 0,
+    })
 
     updateDataObject("test/test2", 2)
 
@@ -39,7 +66,13 @@ export default {
       object[key] = message;
     }
 
-    return {dataObject}
+    const start = () => console.log('start')
+    const stop = () => console.log('stop')
+    const move = ({ x, y, direction, distance }) => {
+      console.log('move', { x, y, direction, distance })
+    }
+
+    return {dataObject, stop, start, move}
   }
 }
 
@@ -47,44 +80,118 @@ export default {
 
 <style scoped>
 
-.app-main{
-  display: grid;
-  grid-template-columns: 0.9fr 1.1fr 1fr;
-  column-gap: 13px;
-  row-gap: 13px;
-  margin: 0.5rem;
-
-  height: 100%;
+*{
+  margin: 0;
+  padding: 0;
 }
+section{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  width: 100%;
 
-::v-deep(.card-title) {
+  background: url("@/assets/wallpaper2.jpg") no-repeat center;
+  background-size: cover;
+}
+.form-box{
+  position: relative;
+  width: 400px;
+  height: 450px;
+  background: transparent;
+  border: 2px solid rgba(255,255,255,0.5);
+  border-radius: 20px;
+  backdrop-filter: blur(15px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+h2{
+  font-size: 2em;
+  color: #fff;
   text-align: center;
-  color: var(--title_color);
-  font-size: 1.2rem;
+}
+.inputbox{
+  position: relative;
+  margin: 30px 0;
+  width: 310px;
+  border-bottom: 2px solid #fff;
+}
+.inputbox label{
+  position: absolute;
+  top: 50%;
+  left: 5px;
+  transform: translateY(-50%);
+  color: #fff;
+  font-size: 1em;
+  pointer-events: none;
+  transition: .5s;
+}
+input:focus ~ label,
+input:valid ~ label{
+  top: -5px;
+}
+.inputbox input {
+  width: 100%;
+  height: 50px;
+  background: transparent;
+  border: none;
+  outline: none;
+  font-size: 1em;
+  padding:0 35px 0 5px;
+  color: #fff;
+}
+.inputbox ion-icon{
+  position: absolute;
+  right: 8px;
+  color: #fff;
+  font-size: 1.2em;
+  top: 20px;
+}
+.forget{
+  margin: -15px 0 15px ;
+  font-size: .9em;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
 }
 
-.card {
-  background: var(--primary-dark);
-  height: 100%;
-  box-shadow: 0 2px 10px 1px rgba(0,0,0,.8);
-}
+.forget label input{
+  margin-right: 3px;
 
-.status-panel {
-  grid-column: 1;
-  grid-row: 1;
 }
-.dashboard {
-  grid-column: 2;
-  grid-row: 1;
+.forget label a{
+  color: #fff;
+  text-decoration: none;
 }
-.ctrl-panel {
-  grid-column: 3;
-  grid-row: 1;
+.forget label a:hover{
+  text-decoration: underline;
 }
-.batt-summ {
-  grid-column: 1;
-  grid-row: 2;
+button{
+  width: 100%;
+  height: 40px;
+  border-radius: 40px;
+  background: #fff;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  font-size: 1em;
+  font-weight: 600;
 }
-
+.register{
+  font-size: .9em;
+  color: #fff;
+  text-align: center;
+  margin: 25px 0 10px;
+}
+.register p a{
+  text-decoration: none;
+  color: #fff;
+  font-weight: 600;
+}
+.register p a:hover{
+  text-decoration: underline;
+}
 
 </style>
